d45eaac96870979047ae31163a19dd2c3371e45c
AlinaV <test@test.ru>
2016-10-31 19:11:15 +1000

    Меню

---
+++ Menu.java
@@ -0,0 +1,11 @@
+package Test;
+
+
+public class Menu {
+    static void main(String args[]){
+        System.out.println("Выберите геометрическую фигуру:");
+        System.out.println("1-треугольник");
+        System.out.println("2-прямоугольник");
+        System.out.println("3-окружность");
+    }
+}
