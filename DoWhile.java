172e5fec5d646af405f887f71e19444e15540578
AlinaV <test@test.ru>
2016-10-31 21:03:48 +1000

    Цикл, который рисует теугольник

---
+++ DoWhile.java
@@ -0,0 +1,13 @@
+package Test;
+
+
+public class DoWhile {
+    public static void main(String args[]) {
+        String s="*";
+        for (int i=0; i<10; i++){
+
+            System.out.println(s);
+            s = s+ "*";
+        }
+    }
+}
