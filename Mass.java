fb6340e01a6d8d90bd050f9717bd397c020e1f03
AlinaV <test@test.ru>
2016-11-02 20:15:28 +1000

    Таблица умножения

---
+++ Mass.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class Mass {
+    public static void main(String args[]) {
+        int [] [] M = new int [10][10];
+        for (int i=1; i<10; i++){
+            for (int j =1;j<10;j++){
+                M[i][j] = i*j;
+                System.out.print(M[i][j]+"\t");
+            }
+            System.out.println("\n");
+        }
+
+
+    }
+}
