8269c199de2477fbe8f70d9b313df804c2cb9436
AlinaV <test@test.ru>
2016-11-09 20:19:35 +1000

    наследование от абстрактного класса

--- Example01oop/Geometry.java
+++ Example01oop/Geometry.java
@@ -7,6 +7,7 @@ public class Geometry {
         rect.b = 20;
         int p = rect.perimetr();
         System.out.println("Периметр прямоугольника="+p);
+        rect.draw();
 
         Triangle trian = new Triangle();
         trian.a = 5;
@@ -14,18 +15,25 @@ public class Geometry {
         trian.c = 6;
         int P = trian.perimetr();
         System.out.println("Периметр треугольника="+P);
+        trian.draw();
     }
 
 }
 
-class Rectangle {
+abstract class Shape {
+    void draw (){
+        System.out.println("Рисуем фигуру");
+    }
+}
+
+class Rectangle extends Shape {
     int a, b;
     int perimetr() {
         return 2 * (a + b);
     }
 }
 
-class Triangle {
+class Triangle extends Shape {
     int a,b,c;
     int perimetr() {
         return a+b+c;
